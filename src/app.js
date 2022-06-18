e3b476b8719ccea02c942b029ba792994bdc05fc
LineJinJ <102582136+LineJinJ@users.noreply.github.com>
2022-06-18 12:30:09 +0200

    I have followed so long, but now I am starting to think it is becoming to advanced to follow

--- src/app.js
+++ src/app.js
@@ -62,6 +62,9 @@ function handleSubmit(event) {
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+  //remove the active class from the celsius link
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
